f445411660e0e8e3454bbbd0136f9f52711b3965
RosarioBA <146356539+RosarioBA@users.noreply.github.com>
2024-05-22 21:29:05 +0200

    deleted files that were unnecessary

--- assets/js/api/constants.mjs
+++
@@ -1,9 +0,0 @@
-export const API_KEY = "cf7dc630-134f-4432-8cca-cc389127181";
-
-export const API_BASE = "https://v2.api.noroff.dev";
-export const API_AUTH = "/auth";
-export const API_REGISTER = "/register";
-export const API_LOGIN = "/login";
-export const API_KEY_URL = "/create-api-key";
-export const API_POSTS = "/blog/posts/<name>";
-
--- assets/js/listeners/auth.mjs
+++
@@ -1,6 +0,0 @@
-import { onAuth } from "../events/onauth.mjs";
-
-export function setAuthListener() {
-    const form = document.getElementById("register-form");
-    form.addEventListener("submit", onAuth);
-}
